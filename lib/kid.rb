21dc51b75a75cc30465628d58d7d663a6fcfbe4f
Rick Belson <belsonrj@aol.com>
2020-03-01 22:47:52 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,14 +2,13 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative 'fancy_dance.rb'
 
-class Kid 
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-  
+class Kid
+  include Dance
   extend MetaDancing
-  
-  include Dance 
-  
-  attr_accessor :name 
-
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
