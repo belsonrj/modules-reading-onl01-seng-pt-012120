320344e1518c9086febb57a94657871b41eb486b
Rick Belson <belsonrj@aol.com>
2020-03-01 22:14:07 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,4 +3,12 @@ require_relative './class_methods_module.rb'
 
 class Kid 
   extend MetaDancing
+  
+  include Dance 
+  
+  attr_accessor :name 
+  
+  def intitialize(name)
+    @name = name
+  end
 end
