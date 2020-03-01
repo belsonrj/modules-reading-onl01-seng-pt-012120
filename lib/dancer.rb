19547c021e57a6d351fae0d6927736fc23428806
Rick Belson <belsonrj@aol.com>
2020-03-01 22:13:50 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,4 +3,12 @@ require_relative './dance_module.rb'
 
 class Dancer
   extend MetaDancing
+  
+  include Dance 
+  
+  attr_accessor :name 
+  
+  def intitialize(name)
+    @name = name
+  end
 end 
